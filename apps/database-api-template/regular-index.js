de4f7a15e008a214a2232fb6a025698b66c68983
Carles Capellas <capellas.carles@gmail.com>
2018-07-31 08:49:30 +0200

    Modify database api template to be run as standalonde express app

---
+++ apps/database-api-template/regular-index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const server = express();
+const bodyParser = require('body-parser');
+const jsonMiddleware = bodyParser.json();
+const Sequelize = require('sequelize');
+const modelsDefinition = require('./models');
+const usersControllerFactory = require('./controllers/users-controller');
+const config = require('./config/config.json');
+
+const dbConnection = new Sequelize(config.db_name, config.db_user, config.db_password, {
+	host: config.db_host,
+	dialect: 'mysql'
+});
+
+const models = modelsDefinition(dbConnection, Sequelize);
+
+dbConnection.sync()
+.then(() => {
+	const usersService = require('./services/users-service')(models);
+	const usersController = usersControllerFactory(usersService);
+
+	router.get('/api/users', usersController.getAll);
+	router.get('/api/users/getById', usersController.getById);
+	router.post('/api/users', jsonMiddleware, usersController.create);
+	router.put('/api/users', jsonMiddleware, usersController.update);
+	router.delete('/api/users', usersController.deleteUser);
+
+	server.use('/database-api-template', router);
+
+	server.listen(80, error => {
+		if (error) {
+			console.error(error);
+		}
+		else {
+			console.log('Express server listening on port 80');
+		}
+	});
+});
