20c6391f450cdecbb1d20d4c918c8ba0aa5161a3
Carles Capellas <capellas.carles@gmail.com>
2018-09-06 18:13:13 +0200

    Update modena dependency version

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const { runServer } = require('modena');
 /* The configuration should be stored in a separate file, not tracked by the source control management */
 const config = {
 	PORT: 80,
-	defaultApp: 'basic-template',
-    sessionSecret: 'ssshhhhh',
-    enableConsoleLogs: 'true',
-    logFilename: 'development.log',
-    tracerLevel: 'log',
+	DEFAULT_APP: 'basic-template',
+    SESSION_SECRET: 'ssshhhhh',
+    ENABLE_CONSOLE_LOGS: 'true',
+    LOG_FILENAME: 'development.log',
+    TRACER_LEVEL: 'log',
 
     'database-api-template_db_host': 'db_host',
     'database-api-template_db_name': 'db_name',
